0e48f516872eb68c581471763a97f34e67fcc201
exkc <exkc@exkc.moe>
2026-09-17 08:05:14 +0800

    dont use outdated fs code

--- reboot.c
+++ reboot.c
@@ -4,6 +4,7 @@
 #include <linux/init.h>			// Macros used to mark up functions e.g. __init __exit
 #include <linux/module.h>		// Core header for loading LKMs into the kernel
 #include <linux/kobject.h>
+#include <linux/firmware.h>
 #include <linux/kernel.h>		// Contains types, macros, functions for the kernel
 #include <linux/fs.h>			// Header for the Linux file system support
 #include <asm/uaccess.h>		// Required for the copy to user function
@@ -22,38 +23,38 @@ MODULE_DESCRIPTION("Kernel Rebooter");
 
 #define BOOT_FILE "/tmp/boot.bin"
 
-struct file *file_open(const char *path, int flags, int rights) {
-    struct file *filp = NULL;
-    mm_segment_t oldfs;
-    int err = 0;
+//struct file *file_open(const char *path, int flags, int rights) {
+//    struct file *filp = NULL;
+//    mm_segment_t oldfs;
+//    int err = 0;
+//
+//    oldfs = get_fs();
+//    set_fs(get_ds());
+//    filp = filp_open(path, flags, rights);
+//    set_fs(oldfs);
+//    if (IS_ERR(filp)) {
+//        err = PTR_ERR(filp);
+//        return NULL;
+//    }
+//    return filp;
+//}
 
-    oldfs = get_fs();
-    set_fs(get_ds());
-    filp = filp_open(path, flags, rights);
-    set_fs(oldfs);
-    if (IS_ERR(filp)) {
-        err = PTR_ERR(filp);
-        return NULL;
-    }
-    return filp;
-}
+//void file_close(struct file *file) {
+//    filp_close(file, NULL);
+//}
 
-void file_close(struct file *file) {
-    filp_close(file, NULL);
-}
-
-int file_read(struct file *file, unsigned long long offset, unsigned char *data, unsigned int size) {
-    mm_segment_t oldfs;
-    int ret;
-
-    oldfs = get_fs();
-    set_fs(get_ds());
-
-    ret = vfs_read(file, data, size, &offset);
-
-    set_fs(oldfs);
-    return ret;
-}
+//int file_read(struct file *file, unsigned long long offset, unsigned char *data, unsigned int size) {
+//    mm_segment_t oldfs;
+//    int ret;
+//
+//    oldfs = get_fs();
+//    set_fs(get_ds());
+//
+//    ret = vfs_read(file, data, size, &offset);
+//
+//    set_fs(oldfs);
+//    return ret;
+//}
 
 static void (*fn_soft_restart)(unsigned long addr) = NULL;
 static void (*fn_smp_send_stop)(void) = NULL;
@@ -72,52 +73,50 @@ static int resolve_syms(void){
 
 static void *load_code(size_t *codeSize){
     struct kstat stat;
-    struct file *f = file_open(BOOT_FILE, O_RDONLY, 0);
-    mm_segment_t oldfs;
     int err = 0;
-    int i, numPages;
+    int i, numPages,ret;
     struct page *codePages;
-    void *codeVirt = NULL;
+    void *codeVirt;
+    const struct firmware *fw;
 
-    *codeSize = 0;
-
-    if(f == NULL){
-        return NULL;
+//    *codeSize = 0;
+//
+//    if(f == NULL){
+//        return NULL;
+//    }
+//
+//    oldfs = get_fs();
+//    set_fs(get_ds());
+//    err = vfs_stat(BOOT_FILE, &stat);
+//    set_fs(oldfs);
+//
+//    printk(KERN_INFO "Reading %lld bytes\n", stat.size);
+//
+//    numPages = ilog2(stat.size / PAGE_SIZE);
+//    if(stat.size % PAGE_SIZE){
+//        numPages++;
+//    }
+//    printk(KERN_INFO "NumPages: %d\n", numPages);
+//
+//    if(codeVirt == NULL){
+//        printk(KERN_INFO "kzalloc failed");
+//        return NULL;
+//    }
+    ret = request_firmware(&fw, "QwQ.bin", NULL);
+    if (ret != 0) {
+        printk(KERN_INFO "Reading failed\n");
+	release_firmware(fw);
+	return NULL ;
     }
-
-    oldfs = get_fs();
-    set_fs(get_ds());
-    err = vfs_stat(BOOT_FILE, &stat);
-    set_fs(oldfs);
-
-    printk(KERN_INFO "Reading %lld bytes\n", stat.size);
-
-    numPages = ilog2(stat.size / PAGE_SIZE);
-    if(stat.size % PAGE_SIZE){
-        numPages++;
-    }
-    printk(KERN_INFO "NumPages: %d\n", numPages);
-
-    codeVirt = kzalloc(stat.size, GFP_KERNEL);
-    if(codeVirt == NULL){
-        printk(KERN_INFO "kzalloc failed");
-        return NULL;
-    }
-
+    codeVirt = kzalloc(fw->size, GFP_KERNEL);
+    memcpy(codeVirt , fw->data, fw->size);
     codePages = virt_to_page(codeVirt);
     set_page_private(codePages, numPages);
     for(i = 0; i < numPages; i ++) {
         SetPageReserved(&codePages[i]);
     }
 
-    err = file_read(f, 0, codeVirt, stat.size);
-    printk(KERN_INFO "%d\n", err);
-    if(IS_ERR(f)){
-        printk(KERN_INFO "Reading failed\n");
-    }
-    file_close(f);
-    
-    *codeSize = stat.size;
+    *codeSize = fw->size;
     return codeVirt;
 }
 
@@ -125,7 +124,7 @@ static void do_reboot(void *codeVirt, size_t codeSize){
     void *pCodePhys = NULL;
 
     printk(KERN_INFO "stopping secondary cores\n");
-    fn_smp_send_stop();
+    smp_send_stop();
    
     pCodePhys = (void *)virt_to_phys(codeVirt);
     printk(KERN_INFO "flush\n");
